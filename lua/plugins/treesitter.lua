f86be706ef8b6c4bf7459fc04ef67919400defda
Guilherme Morum Santos <morum@MacBookPro.local>
2024-09-19 17:36:48 -0300

    ensure golang treesitter support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
@@ -9,7 +7,11 @@ return {
     ensure_installed = {
       "lua",
       "vim",
+      "go",
       -- add more arguments for adding more treesitter parsers
+    },
+    highlight = {
+      enabled = true,
     },
   },
 }
